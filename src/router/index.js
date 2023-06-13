69a85116a3fc56f25b6d6a3c5449f22c3b4e00f8
huangyaqing <714099658@qq.com>
2023-06-13 18:07:52 +0800

    feat:add navbar showMethod

--- src/router/index.js
+++ src/router/index.js
@@ -6,18 +6,23 @@ import Register from "@/container/Register/index.jsx";
 
 const routes = [{
   path: '/',
+  shouldShowNavBar: true,
   component: Home
 }, {
   path: '/bill',
+  shouldShowNavBar: true,
   component: Bill
 }, {
   path: '/login',
+  shouldShowNavBar: false,
   component: Login
 }, {
   path: '/register',
+  shouldShowNavBar: false,
   component: Register
 }, {
   path: '/user',
+  shouldShowNavBar: true,
   component: User
 }]
 
